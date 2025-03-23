280e6abfc717643a18e6bc8f701ce73d39faee3b
Андрій Городецький <elit23520095@ms.sumdu.edu.ua>
2025-03-23 18:09:23 +0300

    12

--- work3task1/work3task1.py
+++ work3task1/work3task1.py
@@ -1,5 +1,5 @@
 text = input("Введіть рядок з кількістью символів більше 15: ")
 while len(text) <= 15:
     text = str(input("Введіть ще раз рядок з кількістью символів більше 15: "))
-slice_text = text[11:14] 
-print("Вибраний зріз:", slice_text)
+slicet = text[11:14] 
+print("Вибраний зріз:", slicet)
